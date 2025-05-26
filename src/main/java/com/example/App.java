7974b70154898ebdef6d74237e62dae09e7f3396
nikimanvi <nikisuresh06@gmail.com>
2025-05-26 19:36:40 +0530

    Git SCM Poll

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! rhis is to test GIT SCM" );
+        System.out.println( "Hello World! rhis is to test GIT SCM and Polling" );
     }
 }
